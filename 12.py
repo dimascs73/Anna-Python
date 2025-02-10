4502c364926c0d7e3295d595b42f84eb9c290f4f
dimascs73 <skus.dima@me.com>
2025-02-10 14:57:15 +0300

    *args, **kwargs

--- 12.py
+++ 12.py
@@ -1,4 +1,14 @@
-days = [input().split() for _ in range(int(input()))]
-dic = {name: (date, month) for name, date, month in days}
+def func(*all, **dic_t):
+    if dic_t:
+        arr: dict = {}
+        arr = {key: val for key, val in dic_t.items()}
+        return arr
+    elif all:
+        lst = []
+        lst = [val for val in all]
+        return lst
 
-print(dic)
+
+print(func("argU", "monEy", "Plus"))
+
+print(func(key1=5, key2=2))
