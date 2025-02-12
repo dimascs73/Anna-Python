2265f46663563d4e11e7e1970f485b8da57598f8
dimascs73 <skus.dima@me.com>
2025-02-12 14:07:56 +0300

    combine_lambda and rename upper_decorator

--- upper_decorator.py
+++ upper_decorator.py
@@ -2,11 +2,11 @@ def up_per(func):
     def u1(*letters):
         uper_case = [str(letter).upper() for letter in letters]
         func(*uper_case)
+
     return u1
+
+
 print = up_per(print)
 
 
-
-
-
-print('Нельзя ли потише?')
+print("Нельзя ли потише?")
