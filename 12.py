b38afbb9278f03871c132f28d39d171a516b5b3d
Dima <skus.dima@me.com>
2025-03-04 21:04:32 +0300

    lucky ticket

--- 12.py
+++ 12.py
@@ -1,4 +1,23 @@
-days = [input().split() for _ in range(int(input()))]
-dic = {name: (date, month) for name, date, month in days}
+def is_lucky_ticket(number):
+    str_num = f'{number:06}'
 
-print(dic)
+    first_half_sum = sum(int(digit) for digit in str_num[:3])
+
+    second_half_sum = sum(int(digit) for digit in str_num[(len(str_num) -1):(len(str_num)-4):-1])
+
+    return first_half_sum == second_half_sum
+
+
+def lucky(ticket):
+    global lastTicket
+
+    current_is_lucky = is_lucky_ticket(ticket)
+    previous_is_lucky = is_lucky_ticket(lastTicket)
+
+    if current_is_lucky and previous_is_lucky:
+        return 'Счастливый'
+    else:
+        return 'Несчастливый'
+    
+lastTicket = 123213
+print(lucky(102111))
