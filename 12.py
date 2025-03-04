433f9789b5e30ca2d02c5ffeb3639335e8e4a2c4
Dima <skus.dima@me.com>
2025-03-04 21:07:27 +0300

    format luky

--- 12.py
+++ 12.py
@@ -1,9 +1,11 @@
 def is_lucky_ticket(number):
-    str_num = f'{number:06}'
+    str_num = f"{number:06}"
 
     first_half_sum = sum(int(digit) for digit in str_num[:3])
 
-    second_half_sum = sum(int(digit) for digit in str_num[(len(str_num) -1):(len(str_num)-4):-1])
+    second_half_sum = sum(
+        int(digit) for digit in str_num[(len(str_num) - 1) : (len(str_num) - 4) : -1]
+    )
 
     return first_half_sum == second_half_sum
 
@@ -15,9 +17,10 @@ def lucky(ticket):
     previous_is_lucky = is_lucky_ticket(lastTicket)
 
     if current_is_lucky and previous_is_lucky:
-        return 'Счастливый'
+        return "Счастливый"
     else:
-        return 'Несчастливый'
-    
+        return "Несчастливый"
+
+
 lastTicket = 123213
 print(lucky(102111))
